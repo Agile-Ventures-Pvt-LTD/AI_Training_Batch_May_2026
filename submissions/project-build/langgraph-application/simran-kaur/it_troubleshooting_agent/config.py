4e1bd30c31ff516d2f806c6eeab1e2d4f168de0b
AG0484-Simran <simran.kaur@agileventures.net>
2026-06-19 19:35:10 +0530

    PROJECT BUILD

---
+++ submissions/project-build/langgraph-application/simran-kaur/it_troubleshooting_agent/config.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()  
+
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_MODEL = os.getenv("GROQ_MODEL", "llama-3.3-70b-versatile")
+DB_PATH = os.getenv("DB_PATH", "data/database/it_support.db")
+KB_PATH = os.getenv("KB_PATH", "data/knowledge_base")
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", "900"))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP", "120"))
---
+++ submissions/project-build/langgraph-application/simran-kaur/it_troubleshooting_agent/db_utils.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+from config import DB_PATH
+
+
+
+
+
+#=============== make connection to database=================
+
+def get_connection():
+
+    return sqlite3.connect(
+        DB_PATH
+    )
+
+
+def extract_schema():
+
+    conn = get_connection()
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    SELECT name
+    FROM sqlite_master
+    WHERE type='table';
+    """)
+
+    tables = cursor.fetchall()
+
+    schema = ""
+
+    for table in tables:
+
+        table_name = table[0]         #table is a tuple ('customers',) . we need to access the first element to get the table name 
+
+        schema += f"\nTABLE: {table_name}\n"
+
+        cursor.execute(
+            f'PRAGMA table_info("{table_name}")'               #PRAGMA is a special command in SQLite that Get column information.  ```PRAGMA table_info(customers)```
+        )
+
+        for column in cursor.fetchall():
+
+            schema += (
+                f"{column[1]} "
+                f"({column[2]})\n"
+            )
+
+    conn.close()
+    return schema
+
